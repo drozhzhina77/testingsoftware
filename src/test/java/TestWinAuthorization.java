a10036eda1c68dac2407a88e6aefee4687c8fa81
drozhzhina77 <87779939+drozhzhina77@users.noreply.github.com>
2023-12-02 08:46:20 +0300

    TestWinAuth edited

--- src/test/java/TestWinAuthorization.java
+++ src/test/java/TestWinAuthorization.java
@@ -6,10 +6,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 // тест окна Войти - проверка авторизации пользователя
 public class TestWinAuthorization {
-    WinAuthorization winAuthorization;
-    JTextField username;
-    JTextField password;
+    private WinAuthorization winAuthorization;
+    private JTextField username;
+    private JTextField password;
 
+    //инициализация окна Войти и его текстовых полей
     public void initWindowAndTextFields() {
         winAuthorization = new WinAuthorization();
         username = new JTextField("", 20);
@@ -17,14 +18,19 @@ public class TestWinAuthorization {
         User.setCurUser(null);
     }
 
+    //задать текст полям для ввода
+    public void setFields(String userText, String passText){
+        username.setText(userText);
+        password.setText(passText);
+        winAuthorization.setUsername(username);
+        winAuthorization.setPassword(password);
+    }
+
     @Test
     // тест на вход с пустыми полями логина и пароля
     public void getUserWithEmptyFieldsTest() throws MyFileException {
         initWindowAndTextFields();
-        username.setText("");
-        password.setText("");
-        winAuthorization.setUsername(username);
-        winAuthorization.setPassword(password);
+        setFields("", "");
         boolean isUser = winAuthorization.getUser();
         assertEquals(isUser, false);
         assertNull(User.getCurUser());
@@ -32,12 +38,9 @@ public class TestWinAuthorization {
 
     @Test
     // тест на вход с пустым полем логина
-    public void getUserWithEmptyFieldUsername() throws MyFileException {
+    public void getUserWithEmptyFieldUsernameTest() throws MyFileException {
         initWindowAndTextFields();
-        username.setText("");
-        password.setText("fBh23k&ju");
-        winAuthorization.setUsername(username);
-        winAuthorization.setPassword(password);
+        setFields("", "fBh23k&ju");
         boolean isUser = winAuthorization.getUser();
         assertEquals(isUser, false);
         assertNull(User.getCurUser());
@@ -45,12 +48,9 @@ public class TestWinAuthorization {
 
     @Test
     // тест на вход с пустым полем пароля
-    public void getUserWithEmptyFieldPassword() throws MyFileException {
+    public void getUserWithEmptyFieldPasswordTest() throws MyFileException {
         initWindowAndTextFields();
-        username.setText("matvey12");
-        password.setText("");
-        winAuthorization.setUsername(username);
-        winAuthorization.setPassword(password);
+        setFields("matvey12","");
         boolean isUser = winAuthorization.getUser();
         assertEquals(isUser, false);
         assertNull(User.getCurUser());
@@ -58,12 +58,9 @@ public class TestWinAuthorization {
 
     @Test
     // тест на вход несуществующего пользователя
-    public void getUserWithIncorrectFields() throws MyFileException {
+    public void getUserWithIncorrectFieldsTest() throws MyFileException {
         initWindowAndTextFields();
-        username.setText("matvey123");
-        password.setText("fBh23k&ju");
-        winAuthorization.setUsername(username);
-        winAuthorization.setPassword(password);
+        setFields("matvey123","fBh23k&ju");
         boolean isUser = winAuthorization.getUser();
         assertEquals(isUser, false);
         assertNull(User.getCurUser());
@@ -71,12 +68,9 @@ public class TestWinAuthorization {
 
     @Test
     // тест на вход существующего пользователя
-    public void getUserWithCorrectFields() throws MyFileException {
+    public void getUserWithCorrectFieldsTest() throws MyFileException {
         initWindowAndTextFields();
-        username.setText("matvey12");
-        password.setText("fBh23k&ju");
-        winAuthorization.setUsername(username);
-        winAuthorization.setPassword(password);
+        setFields("matvey12", "fBh23k&ju");
         boolean isUser = winAuthorization.getUser();
         assertEquals(isUser, true);
         assertNotNull(User.getCurUser());
